89a5f257cd1fb285db78b6178e9418fbf48fdaf4
jiangtianyu <jiangtianyu2009@hotmail.com>
2016-12-01 00:46:46 +0800

    Remove [thz.la] from file names.

--- YouKnowShit/DownloadFilesRename.py
+++ YouKnowShit/DownloadFilesRename.py
@@ -1,12 +1,8 @@
-import requests
-import bs4
 import os
-import urllib.request
-import shutil
 import re
 
 
-distDir = 'F:\\utorrent\\WEST'
+distDir = 'H:\\temp'
 p = re.compile(r'(\D+\d+)\w*(.\w+)')
 
 
@@ -14,12 +10,13 @@ filenames = os.listdir(distDir)
 upperfilenames = []
 print(filenames)
 for filenamepref in filenames:
-    if (filenamepref.find('_') > 0):
+    if filenamepref.find('_') > 0:
         filenameprefit = filenamepref[filenamepref.index('_'):]
     else:
         filenameprefit = filenamepref
     filenamepost = filenameprefit.replace('-', '').replace('_', '')\
-        .replace(' ', '').replace('.1080p', '').replace('.720p', '')
+        .replace(' ', '').replace('.1080p', '').replace('.720p', '')\
+        .replace('[thz.la]', '').replace('[Thz.la]', '')
     distname = p.search(filenamepost).group(1).upper() + p.search(filenamepost).group(2).lower()
     print(distname)
     os.rename(distDir + os.sep + filenamepref, distDir + os.sep + distname)

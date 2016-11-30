1c2daa0e13549d9691ca40e4dab497e309bcb3d6
jiangtianyu <jiangtianyu2009@hotmail.com>
2016-12-01 01:41:09 +0800

    Add ReadBCloudLink.py

---
+++ YouKnowShit/ReadBCloudLink.py
@@ -0,0 +1,14 @@
+import os
+import re
+
+distDir = 'F:\\utorrent\\dllink'
+
+
+filenames = os.listdir(distDir)
+print(filenames)
+
+for filename in filenames:
+    dlurl = "http://pan.baidu.com/s/" + \
+        open(distDir + os.sep + filename, "r").readline().split('/')[4].split(' ')[0]
+    flpwd = open(distDir + os.sep + filename, "r").readline()[-4:]
+    print(dlurl + '  ' + flpwd)

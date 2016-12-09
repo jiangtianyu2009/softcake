3095be6f23a1a2234d562f222cbe8f8d556543f2
jiangtianyu2009 <jiangtianyu2009@hotmail.com>
2016-12-09 14:34:17 +0800

    Display content of uper directory

--- YouKnowShit/CheckFile.py
+++ YouKnowShit/CheckFile.py
@@ -3,17 +3,29 @@ import sys
 
 (dir, filename) = os.path.split(os.path.abspath(sys.argv[0]))
 print(dir)
+filenames = os.listdir(dir)
+for file in filenames:
+    print(file)
+
+print('*****************************************************')
 updir = os.path.abspath('..')
 print(updir)
+filenames = os.listdir(updir)
+for file in filenames:
+    print(file)
+
+print('*****************************************************')
 os.chdir(updir)
 upupdir = os.path.abspath('..')
 print(upupdir)
+filenames = os.listdir(upupdir)
+for file in filenames:
+    print(file)
+
+print('*****************************************************')
 os.chdir(upupdir)
 upupupdir = os.path.abspath('..')
 print(upupupdir)
-print(filename)
-
-
-filenames = os.listdir(dir)
+filenames = os.listdir(upupupdir)
 for file in filenames:
     print(file)

f184b9a664a65e30d8bb0319c9e4bd7518b4f640
jiangtianyu2009 <jiangtianyu2009@hotmail.com>
2016-12-09 14:28:52 +0800

    Get up level directory

--- YouKnowShit/CheckFile.py
+++ YouKnowShit/CheckFile.py
@@ -3,7 +3,16 @@ import sys
 
 (dir, filename) = os.path.split(os.path.abspath(sys.argv[0]))
 print(dir)
+updir = os.path.abspath('..')
+print(updir)
+os.chdir(updir)
+upupdir = os.path.abspath('..')
+print(upupdir)
+os.chdir(upupdir)
+upupupdir = os.path.abspath('..')
+print(upupupdir)
 print(filename)
+
 
 filenames = os.listdir(dir)
 for file in filenames:

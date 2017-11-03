e419fb56874899560f71d8a10b2e1ec02b76a245
jiangtianyu <jiangtianyu2009@hotmail.com>
2017-11-03 10:59:09 +0800

    Support th.new
    Support th.new and th.common, but not sure the coverage.

--- Scrapy/myspider.py
+++ Scrapy/myspider.py
@@ -8,10 +8,17 @@ class QuotesSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        for quote in response.css('th.common'):
+
+        for new in response.css('th.new'):
             yield {
-                'text': quote.css('a.xst::text').extract_first(),
-                'href': quote.css('a.xst::attr("href")').extract_first(),
+                'text': new.css('a.xst::text').extract_first(),
+                'href': new.css('a.xst::attr("href")').extract_first(),
+            }
+
+        for common in response.css('th.common'):
+            yield {
+                'text': common.css('a.xst::text').extract_first(),
+                'href': common.css('a.xst::attr("href")').extract_first(),
             }
 
         next_page = response.css('a.nxt::attr("href")').extract_first()

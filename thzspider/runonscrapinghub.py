cb4f57d0323479cce49e7dd0b76318772b477549
jiangtianyu <jiangtianyu2009@hotmail.com>
2017-11-23 16:31:43 +0800

    Using ScrapingHub Via Py
    Instead using curl cmd, scrapinghub via python client.

---
+++ thzspider/runonscrapinghub.py
@@ -0,0 +1,6 @@
+from scrapinghub import ScrapinghubClient
+
+apikey = '11befd9da9304fecb83dfa114d1926e9'
+client = ScrapinghubClient(apikey)
+project = client.get_project(252342)
+project.jobs.run('thz')

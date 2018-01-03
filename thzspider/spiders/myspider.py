fe6a7569c1daa6c4cc7cb7b98e94a2013bdfb908
jiangtianyu <jiangtianyu2009@hotmail.com>
2018-01-03 10:19:14 +0800

    Extract Para
    Extract para text_new, text_common, href_new, href_common

--- thzspider/spiders/myspider.py
+++ thzspider/spiders/myspider.py
@@ -10,15 +10,21 @@ class ThzSpider(scrapy.Spider):
     def parse(self, response):
 
         for new in response.css('th.new'):
+            text_new = new.css('a.xst::text').extract_first()
+            href_new = 'http://thz.la/' + \
+                new.css('a.xst::attr("href")').extract_first()
             yield {
-                'text': new.css('a.xst::text').extract_first(),
-                'href': 'http://thz.la/' + new.css('a.xst::attr("href")').extract_first(),
+                'text': text_new,
+                'href': href_new,
             }
 
         for common in response.css('th.common'):
+            text_common = common.css('a.xst::text').extract_first()
+            href_common = 'http://thz.la/' + \
+                common.css('a.xst::attr("href")').extract_first()
             yield {
-                'text': common.css('a.xst::text').extract_first(),
-                'href': 'http://thz.la/' + common.css('a.xst::attr("href")').extract_first(),
+                'text': text_common,
+                'href': href_common,
             }
 
         next_page = response.css('a.nxt::attr("href")').extract_first()

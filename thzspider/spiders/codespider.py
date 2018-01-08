449667e48f9747738d92a461a7cdf8be2336450e
jiangtianyu <jiangtianyu2009@hotmail.com>
2018-01-08 14:34:54 +0800

    Comment Print
    Comment print code.

--- thzspider/spiders/codespider.py
+++ thzspider/spiders/codespider.py
@@ -14,7 +14,7 @@ class ThzSpider(scrapy.Spider):
     for job in list(project.jobs.iter_last(spider='javname', state='finished')):
         javjob = job
 
-    print(javjob['key'])
+    #print(javjob['key'])
     job = project.jobs.get(javjob['key'])
 
     namelist = []
@@ -172,7 +172,7 @@ class ThzSpider(scrapy.Spider):
         filters = [("name", "=", [actorname])]
         #print(job.items.list(count=1, filter=filters))
         for item in job.items.iter(count=1, filter=filters):
-            print(baseurl + item['href'])
+            #print(baseurl + item['href'])
             start_urls.append(baseurl + item['href'])
 
     def parse(self, response):

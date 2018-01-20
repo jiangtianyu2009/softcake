22fb2ce9b21765129a4efe201319c4c5d2bf8c55
jiangtianyu <jiangtianyu2009@hotmail.com>
2018-01-20 11:06:27 +0800

    Auto stash before merge of "develop" and "origin/develop"

--- YouKnowShit/DownloadPic.py
+++ YouKnowShit/DownloadPic.py
@@ -5,27 +5,28 @@ import urllib.request
 import shutil
 import re
 
-base_url = 'http://www.j17v.com/cn/vl_searchbyid.php?keyword='
-srcDir = r'F:\tempf\TC'
+base_url = 'http://www.javlibrary.com/tw/vl_searchbyid.php?keyword='
 filterWord = "video_jacket_img"
+srcDirList = [r'E:\download\TC', r'F:\tempf\TC', r'G:\tempg\TC']
 
-filenames = os.listdir(srcDir)
-for filename in filenames:
-    preFileName = filename.split(".")[0]
-    if preFileName[-1] == "A" or preFileName[-1] == "B" or preFileName[-1] == "C":
-        preFileName = preFileName[0:len(preFileName) - 1]
-    destPicName = srcDir + os.sep + preFileName + '.jpg'
-    if os.path.isfile(destPicName):
-        print(destPicName + ' already here.\n')
-    else:
-        full_url = base_url + preFileName
-        response = requests.get(full_url)
-        soup = bs4.BeautifulSoup(response.text, "html.parser")
-        try:
-            imgsrc = 'http:' + soup.find(id=filterWord)['src']
-            print(preFileName + "\n" + imgsrc)
-            print(destPicName + "\n")
-            if not os.path.isfile(destPicName):
-                urllib.request.urlretrieve(imgsrc, destPicName)
-        except:
-            print('Can not find picture of ' + filename + '\n')
+for srcDir in srcDirList:
+    filenames = os.listdir(srcDir)
+    for filename in filenames:
+        preFileName = filename.split(".")[0]
+        if preFileName[-1] == "A" or preFileName[-1] == "B" or preFileName[-1] == "C":
+            preFileName = preFileName[0:len(preFileName) - 1]
+        destPicName = srcDir + os.sep + preFileName + '.jpg'
+        if os.path.isfile(destPicName):
+            print(destPicName + ' already here.\n')
+        else:
+            full_url = base_url + preFileName
+            response = requests.get(full_url)
+            soup = bs4.BeautifulSoup(response.text, "html.parser")
+            try:
+                imgsrc = 'http:' + soup.find(id=filterWord)['src']
+                print(preFileName + "\n" + imgsrc)
+                print(destPicName + "\n")
+                if not os.path.isfile(destPicName):
+                    urllib.request.urlretrieve(imgsrc, destPicName)
+            except:
+                print('!!!!!!!!!!!!!! Can not find picture of ' + filename + '\n')

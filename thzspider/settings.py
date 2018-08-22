0afd4dd779ada36b3f77ec7aba8dca66308e63e1
jiangtianyu <jiangtianyu2008@gmail.com>
2018-08-22 10:44:20 +0800

    Add User Agent
    Add user agent for spiders.

--- thzspider/settings.py
+++ thzspider/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'thzspider.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'thzspider (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True

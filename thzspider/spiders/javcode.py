a9804986526a51b041908c7900481d5d6c73239c
Tianyu Jiang <jiangtianyu2009@hotmail.com>
2020-11-06 14:23:54 +0800

    Update javcode.py

--- thzspider/spiders/javcode.py
+++ thzspider/spiders/javcode.py
@@ -40,8 +40,13 @@ class JavcodeSpider(scrapy.Spider):
         for actorname in JavcodeSpider.namelist:
             filters = [("name", "=", [actorname])]
             for item in job.items.iter(count=1, filter=filters):
-                print(item)
-                actor_url = BASE_URL + item['href']
+                # Network transfer will use bytes data like this:
+                # b'name': b'\xe7\xa7\x8b\xe5\xb1\xb1\xe7\xa5\xa5\xe5\xad\x90',
+                # b'href': b'vl_star.php?s=aqja',
+                if not isinstance(item['href'], str):
+                    actor_url = BASE_URL + str(item['href'], 'utf-8')
+                else:
+                    actor_url = BASE_URL + item['href']
                 print(actorname + '\n' + actor_url)
                 JavcodeSpider.start_urls.append(actor_url)
 

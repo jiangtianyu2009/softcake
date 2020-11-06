2ba7e54db2415e6ade7faceedd8ec44906feb50a
Tianyu Jiang <jiangtianyu2009@hotmail.com>
2020-11-06 16:25:44 +0800

    Update javname.py

--- thzspider/spiders/javname.py
+++ thzspider/spiders/javname.py
@@ -4,23 +4,27 @@ import string
 import requests
 import scrapy
 
+BASE_URL = 'https://www.javlibrary.com/tw/'
+
 
 class JavnameSpider(scrapy.Spider):
     name = "javname"
     start_urls = []
 
     def __init__(self):
-        baseurl = 'http://www.javlibrary.com/tw/'
         for word in string.ascii_uppercase:
             JavnameSpider.start_urls.append(
-                baseurl + 'star_list.php?prefix=' + word)
+                BASE_URL + 'star_list.php?prefix=' + word)
 
     def parse(self, response):
         for searchitem in response.css('div.searchitem'):
+            reference_id = searchitem.css('::attr("id")').extract_first()
+            actor_name = searchitem.css('a::text').extract_first()
+            href_link = searchitem.css('a::attr("href")').extract_first()
             yield {
-                'rfid': searchitem.css('::attr("id")').extract_first(),
-                'name': searchitem.css('a::text').extract_first(),
-                'href': searchitem.css('a::attr("href")').extract_first(),
+                'rfid': reference_id,
+                'name': actor_name,
+                'href': BASE_URL + href_link,
             }
         next_page = response.css('a.next::attr("href")').extract_first()
         if next_page is not None:

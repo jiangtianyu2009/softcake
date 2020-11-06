f725f5ed67b9dce0d7328227fbd51921198a559f
Tianyu Jiang <jiangtianyu2009@hotmail.com>
2020-11-06 13:52:53 +0800

    Update runonscrapinghub.py

--- thzspider/scripts/runonscrapinghub.py
+++ thzspider/scripts/runonscrapinghub.py
@@ -3,7 +3,7 @@ from scrapinghub import ScrapinghubClient
 apikey = '11befd9da9304fecb83dfa114d1926e9'
 client = ScrapinghubClient(apikey)
 project = client.get_project(252342)
-project.jobs.run('javname')
+# project.jobs.run('javname')
 project.jobs.run('javcode')
 # project.jobs.run('thzride')
 # project.jobs.run('myspider')

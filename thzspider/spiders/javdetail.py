e721b7d1187c15796a3ebb6a40ee1c4eb0205bd0
Tianyu Jiang <jiangtianyu2009@hotmail.com>
2020-11-09 14:20:35 +0800

    Update javdetail.py

--- thzspider/spiders/javdetail.py
+++ thzspider/spiders/javdetail.py
@@ -27,26 +27,21 @@ class JavdetailSpider(scrapy.Spider):
         print(javjob['key'])
         job = project.jobs.get(javjob['key'])
 
-        for item in job.items.iter(count=1):
+        for item in job.items.iter():
             # Network transfer will use bytes data like this:
             # b'name': b'\xe7\xa7\x8b\xe5\xb1\xb1\xe7\xa5\xa5\xe5\xad\x90',
             # b'href': b'vl_star.php?s=aqja',
-            print(item)
             if b'link' in item.keys():
                 link_detail = str(item[b'link'], 'utf-8')
             else:
                 link_detail = item['link']
-            print(link_detail)
             JavdetailSpider.start_urls.append(link_detail)
 
     def parse(self, response):
-        print("start parse---------")
         video_id = response.css(
             'div[id="video_id"] .text::text').extract_first()
         video_date = response.css(
             'div[id="video_date"] .text::text').extract_first()
-        print(video_id)
-        print(video_date)
         yield {
             'code': video_id,
             'date': video_date,

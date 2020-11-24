e05a2b572d37a591e58c2040e3eb101f8f361aaf
Tianyu Jiang <jiangtianyu2009@hotmail.com>
2020-11-24 15:54:57 +0800

    Update javorder.py

--- thzspider/spiders/javorder.py
+++ thzspider/spiders/javorder.py
@@ -3,11 +3,7 @@ import requests
 import scrapy
 from scrapinghub import ScrapinghubClient
 
-NAME_LIST_URL = ('https://raw.githubusercontent.com/bsonnier/'
-                 'bsonnier.github.io/master/docs/namelist')
-CODE_FILTER_URL = ('https://raw.githubusercontent.com/bsonnier/'
-                   'bsonnier.github.io/master/docs/codefilter')
-BASE_URL = 'https://www.javlibrary.com/tw/'
+
 TEST_URL = 'https://github.com/'
 API_KEY = '11befd9da9304fecb83dfa114d1926e9'
 PROJECT_ID = '252342'
@@ -19,7 +15,7 @@ class JavorderSpider(scrapy.Spider):
     output = []
 
     def __init__(self):
-        client = ScrapinghubClient(API_KEY)
+        client = ScrapinghubClient(API_KEY, use_msgpack=False)
         project = client.get_project(PROJECT_ID)
 
         # Get detail job
@@ -39,12 +35,8 @@ class JavorderSpider(scrapy.Spider):
         # Sort detail items by date
         tmp_output = []
         for item in detail_job.items.iter():
-            if b'code' in item.keys():
-                code = str(item[b'code'], 'utf-8')
-                date = str(item[b'date'], 'utf-8')
-            else:
-                code = item['code']
-                date = item['date']
+            code = item['code']
+            date = item['date']
             tmp_output.append({'code': code, 'date': date})
         tmp_output.sort(key=lambda x: x['date'], reverse=True)
 
@@ -52,29 +44,18 @@ class JavorderSpider(scrapy.Spider):
         for tmp_item in tmp_output[:1000]:
             filters = [("code", "=", [tmp_item['code']])]
             for item in code_job.items.iter(count=1, filter=filters):
-                if b'code' in item.keys():
-                    code = str(item[b'code'], 'utf-8')
-                    date = tmp_item['date']
-                    text = str(item[b'text'], 'utf-8')
-                    name = str(item[b'name'], 'utf-8')
-                    link = str(item[b'link'], 'utf-8')
-                    down = str(item[b'down'], 'utf-8')
-                    imgs = str(item[b'imgs'], 'utf-8')
-                    imgl = str(item[b'imgl'], 'utf-8')
-                else:
-                    code = item['code']
-                    date = tmp_item['date']
-                    text = item['text']
-                    name = item['name']
-                    link = item['link']
-                    down = item['down']
-                    imgs = item['imgs']
-                    imgl = item['imgl']
+                code = item['code']
+                date = tmp_item['date']
+                text = item['text']
+                name = item['name']
+                link = item['link']
+                down = item['down']
+                imgs = item['imgs']
+                imgl = item['imgl']
             JavorderSpider.output.append({'code': code, 'date': date,
                                           'text': text, 'name': name,
                                           'link': link, 'down': down,
                                           'imgs': imgs, 'imgl': imgl})
-
         JavorderSpider.start_urls.append(TEST_URL)
 
     def parse(self, response):

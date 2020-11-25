286c57d8cb8f8e8a27cb8b451dbe15b1a8c28ffd
Tianyu Jiang <jiangtianyu2009@hotmail.com>
2020-11-25 14:10:38 +0800

    Update settings.py

--- thzspider/settings.py
+++ thzspider/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'thzspider.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False

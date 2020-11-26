778f1e28719503be54d0532c9558b7861da23266
Tianyu Jiang <jiangtianyu2009@hotmail.com>
2020-11-26 10:57:01 +0800

    Update javdetail.py

--- thzspider/spiders/javdetail.py
+++ thzspider/spiders/javdetail.py
@@ -16,23 +16,27 @@ class JavdetailSpider(scrapy.Spider):
         client = ScrapinghubClient(API_KEY, use_msgpack=False)
         project = client.get_project(PROJECT_ID)
 
-        for job in list(project.jobs.iter_last(
+        for jav_code_job in list(project.jobs.iter_last(
                 spider='javcode', state='finished')):
-            javjob = job
+            jav_job = jav_code_job
 
-        print(javjob['key'])
-        job = project.jobs.get(javjob['key'])
+        print(jav_job['key'])
+        jav_code_job = project.jobs.get(jav_job['key'])
 
-        for item in job.items.iter():
+        for item in jav_code_job.items.iter():
             link_detail = item['link']
             JavdetailSpider.start_urls.append(link_detail)
+
+        JavdetailSpider.start_urls = list(set(JavdetailSpider.start_urls))
 
     def parse(self, response):
         video_id = response.css(
             'div[id="video_id"] .text::text').extract_first()
         video_date = response.css(
             'div[id="video_date"] .text::text').extract_first()
+        video_maker = response.css('span.maker .a::text').extract_first()
         yield {
             'code': video_id,
             'date': video_date,
+            'makr': video_maker,
         }

af5a11febfe9e24358aeb93355f8fa12709e9c6e
Tianyu Jiang <jiangtianyu2009@hotmail.com>
2020-12-09 15:38:26 +0800

    Delete files

--- localjob/ReadFavstars.py
+++
@@ -1,9 +0,0 @@
-import bs4
-
-favstarsFile = r'C:\Users\JIANG\Downloads\favstars-mjyang-1461674723.html'
-favstarsList = []
-htmlfile = open(favstarsFile, 'r', encoding='UTF-8')
-htmltext = htmlfile.read()
-soup = bs4.BeautifulSoup(htmltext, "html.parser")
-for actName in soup.select('span'):
-    favstarsList.append(actName.string)

68103aca5e0f9bc4babb120be9f9f3c0819a033c
Tianyu Jiang <jiangtianyu2009@hotmail.com>
2021-03-08 23:41:13 +0800

    Update nhtchn.py

--- thzspider/spiders/nhtchn.py
+++ thzspider/spiders/nhtchn.py
@@ -18,18 +18,35 @@ class NhtchnSpider(scrapy.Spider):
 
         NhtchnSpider.start_urls.append(BASE_URL)
 
+# div.contain .blue{color:blue;}
+# div.contain.blue{color:blue;}
+# 以上两种规则分别应用的元素如下：
+
+# 1
+#   <!--后代-->
+#   <div class="contain">
+#      contain
+#   <span class="blue">blue</span>
+#   </div>
+# 2
+# <!--多类-->
+# <div class="contain blue">contain and blue</div>
+
     def parse(self, response):
         for codeitem in response.css('div.container.index-container'):
             for galleryitem in codeitem.css('div.gallery'):
                 gallery_href = galleryitem.css('a::attr(href)').extract_first()
                 gallery_caption = galleryitem.css(
-                    'a .div.caption::text').extract_first()
+                    'a .caption::text').extract_first()
+                gallery_thumb = galleryitem.css(
+                    'a .lazyload::attr(data-src)').extract_first()
                 # id_code = codeitem.css('div.id::text').extract_first()
                 # href_link = codeitem.css('a::attr(href)').extract_first()
                 # img_small = codeitem.css('img::attr(src)').extract_first()
                 yield {
                     'href': gallery_href,
                     'capt': gallery_caption,
+                    'thmb': gallery_thumb,
                 }
         # next_page = response.css('a.next::attr("href")').extract_first()
         # if next_page is not None:
